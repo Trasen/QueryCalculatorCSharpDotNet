25346aa88f86a8aaba32b012cb81e9b4b460175d
trasen <felixzetterlund@gmail.com>
2021-10-07 18:07:56 +0200

    * Progress save

--- QueryCalculator/Calculator/CalculatorImpl.cs
+++ QueryCalculator/Calculator/CalculatorImpl.cs
@@ -98,31 +98,4 @@ namespace QueryCalculator.Calculator
             return calculation.getResult();
         }
     }
-
-    internal class NestedCalculationTracker : OperatorTracker
-    {
-        public NestedCalculationTracker(string query)
-        {
-            for (int i = 0; i < query.Length; i++)
-            {
-                char ch = query[i];
-
-                if (ch == '(')
-                {
-                    indexStart = i;
-                }
-
-                if (ch == ')')
-                {
-                    indexEnd = i;
-                    break;
-                }
-            }
-        }
-
-        public bool IsComplete()
-        {
-            return !(indexStart == -1 || indexEnd == -1);;
-        }
-    }
 }
---
+++ QueryCalculator/Calculator/NestedCalculationTracker.cs
@@ -0,0 +1,31 @@
+﻿using QueryCalculator.Calculator.Calculations;
+
+namespace QueryCalculator.Calculator
+{
+    internal class NestedCalculationTracker : OperatorTracker
+    {
+        public NestedCalculationTracker(string query)
+        {
+            for (int i = 0; i < query.Length; i++)
+            {
+                char ch = query[i];
+
+                if (ch == '(')
+                {
+                    indexStart = i;
+                }
+
+                if (ch == ')')
+                {
+                    indexEnd = i;
+                    break;
+                }
+            }
+        }
+
+        public bool IsComplete()
+        {
+            return !(indexStart == -1 || indexEnd == -1);;
+        }
+    }
+}
