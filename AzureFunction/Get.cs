7e4bca65922495b8deb3d9156029c4440a3c2741
trasen <felixzetterlund@gmail.com>
2021-10-11 12:57:41 +0200

    * Dep cleanup

--- AzureFunction/Get.cs
+++ AzureFunction/Get.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.IO;
+﻿
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 using QueryCalculator.Calculator;
 
 namespace AzureFunction
@@ -16,15 +11,13 @@ namespace AzureFunction
     public static class Get
     {
         [FunctionName("Get")]
-        public static async Task<string> RunAsync(
+        public static string RunAsync(
             [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)]
             HttpRequest req, ILogger log)
         {
             Calculator calculator = new CalculatorImpl();
             string response = calculator.calculate(req.Query.First().Key);
             return response;
-
-            calculator.calculate("asd");
         }
     }
 }
