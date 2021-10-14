c65dae33a9070cc0caddb198fed8c7f58bf013e4
trasen <felixzetterlund@gmail.com>
2021-10-14 09:55:04 +0200

    * Fixes for Docker

--- API/Startup.cs
+++ API/Startup.cs
@@ -27,23 +27,23 @@ namespace DotNetAPIDefinition
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo {Title = "API", Version = "v1"});
-            });
+            services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "API", Version = "v1"}); });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             //if (env.IsDevelopment())
-        //    {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
-          //  }
+            //    {
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+            //  }
 
-            app.UseHttpsRedirection();
+            if (!env.IsDevelopment())
+            {
+                app.UseHttpsRedirection();
+            }
 
             app.UseRouting();
 
--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,19 @@
 ﻿FROM mcr.microsoft.com/dotnet/core/aspnet:3.1 AS base
 WORKDIR /app
-EXPOSE 5000
-EXPOSE 5001
+EXPOSE 80
+EXPOSE 443
 
-ENV ASPNETCORE_URLS="https://*:5000"
-
+#ENV ASPNETCORE_URLS="https://+;http://+"
+ENV ASPNETCORE_URLS="http://+"
+ENV ASPNETCORE_ENVIRONMENT=Development
+#ENV ASPNETCORE_Kestrel__Certificates__Default__Path=/https/aspnetapp.pfx
+#ENV ASPNETCORE_Kestrel__Certificates__Default__Password="9900"
 FROM mcr.microsoft.com/dotnet/core/sdk:3.1 AS build
 
 COPY ["/", "/querycalculator"]
-
+#RUN dotnet dev-certs https -ep /https/aspnetapp.pfx -p "9900"
+#RUN update-ca-certificates
+#RUN dotnet dev-certs https  --trust
 RUN dotnet test "/querycalculator/QueryCalculator/"
 COPY . .
 
