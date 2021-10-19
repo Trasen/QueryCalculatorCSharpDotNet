2bae21521ebe202650de9aff4edadcafde81eafd
trasen <felixzetterlund@gmail.com>
2021-10-19 12:36:27 +0200

    * Still trying to fix the AWS intigration - it's acting a bit strange - the handler can't be traced.

---
+++ Lambda/Function.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Amazon.Lambda.Core;
+using QueryCalculator.Calculator;
+
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace Lambda
+{
+    public class Function
+    {
+        
+        public string FunctionHandler(string input, ILambdaContext context)
+        {
+            Calculator calculator = new CalculatorImpl();
+            return calculator.calculate(input);
+        }
+    }
+}
+
+//QueryCalculator::Lambda.Function::FunctionHandler
